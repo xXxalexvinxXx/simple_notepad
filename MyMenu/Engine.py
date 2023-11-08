bd1206a35f9222fe4919f829a90803dab93071a1
Al Vin <zdejh.gm@gmail.com>
2023-11-08 08:49:21 +0300

    add menu

---
+++ MyMenu/Engine.py
@@ -0,0 +1,23 @@
+from MyMenu import MyMenu as menu
+
+
+def start():
+    while True:
+        menu.draw_menu()
+        user_input = input()
+        if user_input == '1':
+            print("all")
+        elif user_input == '2':
+            print("ID")
+        elif user_input == '3':
+            print("date")
+        elif user_input == '4':
+            print("all")
+        elif user_input == '5':
+            print("note")
+        elif user_input == '6':
+            print("all")
+        else:
+            print("Программа Журнал заметок завершена")
+            break
+
